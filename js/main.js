7ed9bbe497537974c0e8754d871352e56ad5856e
Erza Qorri <erza.qorri@student.uni-pr.edu>
2021-12-28 21:03:49 +0100

    Adding clock using Canvas

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ function showSlides(n) {
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
+ 
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
